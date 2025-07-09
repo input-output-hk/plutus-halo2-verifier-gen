810903aa21c9ddc5828ce883e7bfdf37924f908b
Adam Smolarek <adam.smolarek@iohk.io>
2025-07-09 12:04:37 +0200

    update tests

--- plutus-verifier/plutus-halo2/test/Lagrange.hs
+++ plutus-verifier/plutus-halo2/test/Lagrange.hs
@@ -12,13 +12,14 @@ test :: Tasty.TestTree
 test =
     Tasty.testGroup
         "unit tests"
-        [ Tasty.testCase "interpolation for (1,1) eval at 1" proofTest
-        , Tasty.testCase "interpolation for (1,1) (2,2) eval at 4" proofTest1
-        , Tasty.testCase "interpolation for (3476578436,435897394) (676324645,32465456672) eval at 42" proofTest1
+        [ Tasty.testCase "interpolation for (1,1) eval at 1" existingPoint
+        , Tasty.testCase "interpolation for (1,1) (2,2) eval at 4" linearCase
+        , Tasty.testCase "interpolation for 2 points provided, eval at 42" biggerNumber
+        , Tasty.testCase "interpolation for 4 points provided, eval at 42" nonLinearCase
         ]
 
-proofTest :: Tasty.Assertion
-proofTest = do
+existingPoint :: Tasty.Assertion
+existingPoint = do
     let
         x1 = mkScalar 1
         y1 = mkScalar 1
@@ -27,26 +28,55 @@ proofTest = do
         result = lagrangeInPlace [(x1, y1)] x
     result @?= expected
 
-proofTest1 :: Tasty.Assertion
-proofTest1 = do
+linearCase :: Tasty.Assertion
+linearCase = do
     let
         x1 = mkScalar 1
         y1 = mkScalar 1
+
         x2 = mkScalar 2
         y2 = mkScalar 2
+
         x = mkScalar 4
         expected = mkScalar 4
         result = lagrangeInPlace [(x1, y1), (x2, y2)] x
     result @?= expected
 
-proofTest2 :: Tasty.Assertion
-proofTest2 = do
+biggerNumber :: Tasty.Assertion
+biggerNumber = do
     let
         x1 = mkScalar 3476578436
         y1 = mkScalar 676324645
+
         x2 = mkScalar 676324645
         y2 = mkScalar 32465456672
+
         x = mkScalar 42
-        expected = mkScalar 50005543427641638138774391874998187797399605664044700965529451695707093553735
+        expected = mkScalar 0x3a7293683d1b89a083804afca51b323fa62924d60f0a40d3e46e6bc5d17ca858
         result = lagrangeInPlace [(x1, y1), (x2, y2)] x
     result @?= expected
+
+
+nonLinearCase :: Tasty.Assertion
+nonLinearCase = do
+     let
+         x1 = mkScalar 6763246453476578436
+         y1 = mkScalar 2465456672435897394
+
+         x2 = mkScalar 3476578436676324645
+         y2 = mkScalar 3589739432465456672
+
+         x3 = mkScalar 8796067088957874756
+         y3 = mkScalar 3623657456997093465
+
+         x4 = mkScalar 9686764663489690657
+         y4 = mkScalar 7957456354576897947
+
+         x = mkScalar 42
+         expected = mkScalar 0x3afdcbe845065bf92428e7d4f1b41856674f05fa9d1c73234cb15a8c1fc43663
+
+         result = lagrangeInPlace [(x1, y1), (x2, y2), (x3, y3), (x4, y4)] x
+         unorderedResult =  lagrangeInPlace [(x3, y3), (x1, y1), (x4, y4), (x2, y2)] x
+
+     unorderedResult @?= expected
+     result @?= expected
