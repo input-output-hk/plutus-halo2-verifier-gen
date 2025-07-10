36ed94f512edd623cfa5a0cd7ab6efffa66d288b
Adam Smolarek <adam.smolarek@iohk.io>
2025-07-10 17:46:56 +0200

    wip buildMSM for new KZG

---
+++ plutus-verifier/plutus-halo2/src/Plutus/Crypto/Halo2/MultiOpenMSM.hs
@@ -0,0 +1,112 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Plutus.Crypto.Halo2.MultiOpenMSM where
+
+import Plutus.Crypto.BlsTypes (
+    Scalar,
+    recip,
+ )
+import Plutus.Crypto.BlsUtils (
+    powers,
+ )
+import Plutus.Crypto.Halo2.LagrangePolynomialEvaluation (
+    lagrangeInPlace,
+ )
+import Plutus.Crypto.Halo2.MSMTypes (
+    MSM (..),
+    MSMElem (MSMElem),
+    appendTerm,
+ )
+import PlutusTx.Builtins (
+    BuiltinBLS12_381_G1_Element,
+ )
+import PlutusTx.List (
+    filter,
+    find,
+    foldl,
+    head,
+    length,
+    map,
+    unzip,
+    zip,
+ )
+import PlutusTx.Prelude (
+    Bool (False, True),
+    Integer,
+    one,
+    zero,
+    (*),
+    (+),
+    (-),
+ )
+
+-- this function takes commitment with point sets and returns left and right MSM for further evaluation
+{-# INLINEABLE buildMSM #-}
+buildMSM ::
+    Scalar ->
+    Scalar ->
+    Scalar ->
+    Scalar ->
+    [Scalar] ->
+    [(BuiltinBLS12_381_G1_Element, Integer, [Scalar], [Scalar])] ->
+    [[Scalar]] ->
+    (MSM, MSM)
+buildMSM x1 x2 x3 x4 proofX3QEvals commitmentMap pointSets = (MSM [], MSM [])
+  where
+    pointSetsIndexes :: [Integer]
+    pointSetsIndexes = [0 .. (length pointSets - 1)]
+
+    x1Powers :: [Scalar]
+    x1Powers = x1 : [x1 * x | x <- x1Powers]
+
+    result =
+        map
+            ( \set_index ->
+                let
+                    -- all commitments for given index
+                    commitmentsForIndex :: [(BuiltinBLS12_381_G1_Element, Integer, [Scalar], [Scalar])]
+                    commitmentsForIndex =
+                        filter
+                            ( \c -> case c of
+                                (_, set_index, _, _) -> True
+                                _ -> False
+                            )
+                            commitmentMap
+
+                    -- calculate inner products for commitments and for evaluations
+                    comm =
+                        foldl
+                            (\msm (x1Power, (c, _, _, _)) -> appendTerm msm (MSMElem (x1Power, c)))
+                            (MSM [])
+                            (zip x1Powers commitmentsForIndex)
+
+                    eval_set =
+                        -- this multiply all elements from evaluation list es and sums up result
+                        map
+                            ( \(x1Power, (_, _, _, es)) ->
+                                foldl
+                                    ( \acc evaluation ->
+                                        acc + evaluation * x1Power
+                                    )
+                                    (zero :: Scalar)
+                                    es
+                            )
+                            (zip x1Powers commitmentsForIndex)
+                 in
+                    (comm, eval_set)
+            )
+            pointSetsIndexes
+    (q_coms, q_eval_sets) = unzip result
+
+    f_eval =
+        foldl
+            ( \accEval ((points, evals), proofQEval) ->
+                let
+                    rEval = lagrangeInPlace (zip points evals) x3
+                    den = foldl (\acc point -> acc * (x3 - point)) one points
+                    eval = (proofQEval - rEval) * (recip den)
+                 in
+                    accEval * x2 + eval
+            )
+            zero
+            (zip (zip pointSets q_eval_sets) proofX3QEvals)
--- plutus-verifier/plutus-halo2/test/Lagrange.hs
+++ plutus-verifier/plutus-halo2/test/Lagrange.hs
@@ -56,27 +56,26 @@ biggerNumber = do
         result = lagrangeInPlace [(x1, y1), (x2, y2)] x
     result @?= expected
 
-
 nonLinearCase :: Tasty.Assertion
 nonLinearCase = do
-     let
-         x1 = mkScalar 6763246453476578436
-         y1 = mkScalar 2465456672435897394
+    let
+        x1 = mkScalar 6763246453476578436
+        y1 = mkScalar 2465456672435897394
 
-         x2 = mkScalar 3476578436676324645
-         y2 = mkScalar 3589739432465456672
+        x2 = mkScalar 3476578436676324645
+        y2 = mkScalar 3589739432465456672
 
-         x3 = mkScalar 8796067088957874756
-         y3 = mkScalar 3623657456997093465
+        x3 = mkScalar 8796067088957874756
+        y3 = mkScalar 3623657456997093465
 
-         x4 = mkScalar 9686764663489690657
-         y4 = mkScalar 7957456354576897947
+        x4 = mkScalar 9686764663489690657
+        y4 = mkScalar 7957456354576897947
 
-         x = mkScalar 42
-         expected = mkScalar 0x3afdcbe845065bf92428e7d4f1b41856674f05fa9d1c73234cb15a8c1fc43663
+        x = mkScalar 42
+        expected = mkScalar 0x3afdcbe845065bf92428e7d4f1b41856674f05fa9d1c73234cb15a8c1fc43663
 
-         result = lagrangeInPlace [(x1, y1), (x2, y2), (x3, y3), (x4, y4)] x
-         unorderedResult =  lagrangeInPlace [(x3, y3), (x1, y1), (x4, y4), (x2, y2)] x
+        result = lagrangeInPlace [(x1, y1), (x2, y2), (x3, y3), (x4, y4)] x
+        unorderedResult = lagrangeInPlace [(x3, y3), (x1, y1), (x4, y4), (x2, y2)] x
 
-     unorderedResult @?= expected
-     result @?= expected
+    unorderedResult @?= expected
+    result @?= expected
