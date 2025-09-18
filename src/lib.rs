dcaa67f5b504ce51e1ce00dd73dad90c0aebc6eb
Adam Smolarek <adam.smolarek@iohk.io>
2025-09-18 15:53:18 +0200

    expose useful dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
 pub mod circuits;
 pub mod plutus_gen;
+
+pub use halo2_proofs::{
+    plonk::{ProvingKey, VerifyingKey, create_proof, k_from_circuit, keygen_pk, keygen_vk},
+    poly::{gwc_kzg::GwcKZGCommitmentScheme, kzg::params::ParamsKZG},
+    transcript::{CircuitTranscript, Transcript},
+};
