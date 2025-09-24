e1c744201b4fc5e8ab05abddcb5e1d4c126f5967
Adam Smolarek <adam.smolarek@iohk.io>
2025-09-24 22:39:22 +0200

    expose schnorr sig type

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod circuits;
 pub mod plutus_gen;
 
+pub use atms_halo2::signatures::schnorr::SchnorrSig;
 pub use halo2_proofs::{
     plonk::{ProvingKey, VerifyingKey, create_proof, k_from_circuit, keygen_pk, keygen_vk},
     poly::{gwc_kzg::GwcKZGCommitmentScheme, kzg::params::ParamsKZG},
