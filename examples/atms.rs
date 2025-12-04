9715f6fdf1cbe1dedd977b0f3e1c5efb645ccd17
Nicolas Di Prima <nicolas@primetype.co.uk>
2025-12-04 15:55:38 +0000

    Update the error handling.
    This allows to go from an error message to look like this:
    
    ```
    thread 'main' (9280059) panicked at src/api.rs:183:14:
    Failed to read .env file: Io(Custom { kind: NotFound, error: "path not found" })
    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
    ```
    
    To an error message that looks like this:
    
    ```
    Error: Failed to load necessary environment variables
    
    Caused by:
        0: Failed to read .env file
        1: path not found
        2: path not found
    ```
    
    Now the load_env function does not report a useful error and maybe
    it should be handled differently. But this is not the point of this PR.

--- examples/atms.rs
+++ examples/atms.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context as _, Result, anyhow, bail};
 use blstrs::{Base, Bls12, G1Projective, Scalar};
 use halo2_proofs::{
     plonk::{
@@ -11,6 +12,7 @@ use halo2_proofs::{
 };
 use log::info;
 use plutus_halo2_verifier_gen::plutus_gen::extraction::ExtractKZG;
+use plutus_halo2_verifier_gen::plutus_gen::generate_aiken_verifier;
 use plutus_halo2_verifier_gen::{
     circuits::atms_circuit::{AtmsSignatureCircuit, prepare_test_signatures},
     plutus_gen::{
@@ -22,18 +24,15 @@ use rand::prelude::StdRng;
 use rand_core::SeedableRng;
 use std::env;
 use std::fs::File;
-use plutus_halo2_verifier_gen::plutus_gen::generate_aiken_verifier;
 
-fn main() {
+fn main() -> Result<()> {
     env_logger::init_from_env(env_logger::Env::default().filter_or("RUST_LOG", "info"));
     let args: Vec<String> = env::args().collect();
 
     match &args[1..] {
-        [] => {
-            compile_atms_circuit::<KZGCommitmentScheme<Bls12>>();
-        }
+        [] => compile_atms_circuit::<KZGCommitmentScheme<Bls12>>(),
         [command] if command == "gwc_kzg" => {
-            compile_atms_circuit::<GwcKZGCommitmentScheme<Bls12>>();
+            compile_atms_circuit::<GwcKZGCommitmentScheme<Bls12>>()
         }
         _ => {
             println!("Usage:");
@@ -41,6 +40,8 @@ fn main() {
             println!(
                 "- to run the example using the GWC19 version of multi-open KZG, run: `cargo run --example example_name gwc_kzg`"
             );
+
+            bail!("Invalid command line arguments")
         }
     }
 }
@@ -52,7 +53,7 @@ pub fn compile_atms_circuit<
             Parameters = ParamsKZG<Bls12>,
             VerifierParameters = ParamsVerifierKZG<Bls12>,
         > + ExtractKZG,
->() {
+>() -> Result<()> {
     let seed = [0u8; 32]; // UNSAFE, constant seed is used for testing purposes
     let mut rng: StdRng = SeedableRng::from_seed(seed);
 
@@ -82,8 +83,8 @@ pub fn compile_atms_circuit<
 
     let instances_file =
         "./plutus-verifier/plutus-halo2/test/Generic/serialized_public_input.hex".to_string();
-    let mut output = File::create(instances_file).expect("failed to create instances file");
-    export_public_inputs(instances, &mut output);
+    let mut output = File::create(instances_file).context("failed to create instances file")?;
+    export_public_inputs(instances, &mut output).context("Failed to export the public inputs")?;
 
     let mut transcript: CircuitTranscript<CardanoFriendlyState> =
         CircuitTranscript::<CardanoFriendlyState>::init();
@@ -96,7 +97,7 @@ pub fn compile_atms_circuit<
         &mut rng,
         &mut transcript,
     )
-    .expect("proof generation should not fail");
+    .context("proof generation should not fail")?;
 
     let proof = transcript.finalize();
     info!("proof size {:?}", proof.len());
@@ -109,21 +110,24 @@ pub fn compile_atms_circuit<
         instances,
         &mut transcript_verifier,
     )
-    .expect("prepare verification failed");
+    .context("prepare verification failed")?;
 
     verifier
         .verify(&kzg_params.verifier_params())
-        .expect("verify failed");
+        .map_err(|e| anyhow!("{e:?}"))
+        .context("verify failed")?;
 
     serialize_proof(
         "./plutus-verifier/plutus-halo2/test/Generic/serialized_proof.json".to_string(),
         proof.clone(),
     )
-    .expect("json proof serialization failed");
+    .context("json proof serialization failed")?;
 
     generate_plinth_verifier(&kzg_params, &vk, instances)
-        .expect("Plinth verifier generation failed");
+        .context("Plinth verifier generation failed")?;
 
     generate_aiken_verifier(&kzg_params, &vk, instances, Some(proof))
-        .expect("Aiken verifier generation failed");
+        .context("Aiken verifier generation failed")?;
+
+    Ok(())
 }
