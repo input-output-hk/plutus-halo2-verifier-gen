a6ae61a443179272e8b9f81701921168ef5c968a
Adam Smolarek <adam.smolarek@iohk.io>
2025-12-17 11:34:48 +0100

    apply contract to unit

--- profile.sh
+++ profile.sh
@@ -7,7 +7,7 @@ xxd -r -p < contract.hex > contract.cbor
 SKIP=3
 
 dd if=contract.cbor of=script.flat bs=1 skip=$SKIP 2>/dev/null
-
-nix run github:IntersectMBO/plutus/1.55.0.0#uplc -- evaluate -t -i script.flat --if flat --trace-mode LogsWithBudgets -o logs
+nix run github:IntersectMBO/plutus/1.55.0.0#uplc -- apply --if flat script.flat unit.flat --of flat -o script2.flat
+nix run github:IntersectMBO/plutus/1.55.0.0#uplc -- evaluate -t -i script2.flat --if flat --trace-mode LogsWithBudgets -o logs
 cat logs | nix run github:IntersectMBO/plutus/1.30.0.0#traceToStacks | nix run nixpkgs#flamegraph > cpu.svg
 cat logs | nix run github:IntersectMBO/plutus/1.30.0.0#traceToStacks -- --column 2 | nix run nixpkgs#flamegraph > mem.svg``
