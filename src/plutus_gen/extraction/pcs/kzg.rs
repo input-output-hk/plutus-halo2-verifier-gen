ce003267efbc67ce0be471a6e5ef7f94e58bad9d
Raphael Toledo <raphael.toledo@iohk.io>
2026-02-03 17:05:36 +0000

    fixpup extract_q

--- src/plutus_gen/extraction/pcs/kzg.rs
+++ src/plutus_gen/extraction/pcs/kzg.rs
@@ -93,7 +93,7 @@ impl CircuitRepresentation {
         // number of final witnesses is equal to number of different point sets
         let (sets, _) = self.precompute_intermediate_sets();
         let number_of_witnesses = sets.len();
-        circuit_repr.extract_q_evaluations_count(number_of_witnesses);
+        self.extract_q_evaluations_count(number_of_witnesses);
 
         // witnesses
         for _ in 0..number_of_witnesses {
