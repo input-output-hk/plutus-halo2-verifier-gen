54221e39f625f69fac8dea25c1835c66d88926cf
Raphael Toledo <raphael.toledo@iohk.io>
2026-02-05 10:33:02 +0000

    Replacing impl CircuitRepr. when possible

--- src/plutus_gen/extraction/data/expression_steps/proof.rs
+++ src/plutus_gen/extraction/data/expression_steps/proof.rs
@@ -11,113 +11,113 @@ use halo2_proofs::halo2curves::group::prime::PrimeCurveAffine;
 #[cfg(feature = "plutus_debug")]
 use log::info;
 
-impl<PCS: ExtractPCS> CircuitRepresentation<PCS> {
-    pub fn extract_proof_steps(&mut self, vk: &VerifyingKey<Scalar, PCS>) -> ()
-    where
-        PCS: PolynomialCommitmentScheme<Scalar, Commitment = G1Projective>,
-    {
-        let chunk_len = vk.cs().degree() - 2;
+pub fn extract_proof_steps<PCS>(
+    circuit_repr: &mut CircuitRepresentation<PCS>,
+    vk: &VerifyingKey<Scalar, PCS>,
+) where
+    PCS: PolynomialCommitmentScheme<Scalar, Commitment = G1Projective> + ExtractPCS,
+{
+    let chunk_len = vk.cs().degree() - 2;
 
-        let mut advice_commitments = vec![G1Affine::generator(); vk.cs().num_advice_columns()];
-        let mut challenges = vec![Scalar::ZERO; vk.cs().num_challenges()];
+    let mut advice_commitments = vec![G1Affine::generator(); vk.cs().num_advice_columns()];
+    let mut challenges = vec![Scalar::ZERO; vk.cs().num_challenges()];
 
-        let all_phases = vk.cs().advice_column_phase();
-        let max_phase = all_phases
+    let all_phases = vk.cs().advice_column_phase();
+    let max_phase = all_phases
+        .iter()
+        .max()
+        .expect("No max_phase for phases found");
+    let all_phases = 0..=(*max_phase);
+
+    #[cfg(feature = "plutus_debug")]
+    info!(
+        "proofs: vk phases{} ?= all phases {}",
+        Vec::from_iter(vk.cs().phases()).len(),
+        all_phases.len()
+    );
+
+    for current_phase in all_phases {
+        for (phase, _commitment) in vk
+            .cs()
+            .advice_column_phase()
             .iter()
-            .max()
-            .expect("No max_phase for phases found");
-        let all_phases = 0..=(*max_phase);
-
-        #[cfg(feature = "plutus_debug")]
-        info!(
-            "proofs: vk phases{} ?= all phases {}",
-            Vec::from_iter(vk.cs().phases()).len(),
-            all_phases.len()
-        );
-
-        for current_phase in all_phases {
-            for (phase, _commitment) in vk
-                .cs()
-                .advice_column_phase()
-                .iter()
-                .zip(advice_commitments.iter_mut())
-            {
-                if current_phase == *phase {
-                    self.extract_step(ProofExtractionSteps::AdviceCommitments);
-                }
-            }
-            for (phase, _challenge) in vk.cs().challenge_phase().iter().zip(challenges.iter_mut()) {
-                if current_phase == *phase {
-                    self.extract_step(ProofExtractionSteps::SqueezeChallenge);
-                }
+            .zip(advice_commitments.iter_mut())
+        {
+            if current_phase == *phase {
+                circuit_repr.extract_step(ProofExtractionSteps::AdviceCommitments);
             }
         }
-
-        self.extract_step(ProofExtractionSteps::Theta);
-
-        let nb_lookups = vk.cs().lookups().len();
-        (0..nb_lookups).for_each(|_argument| {
-            self.extract_step(ProofExtractionSteps::LookupPermuted);
-        });
-
-        self.extract_step(ProofExtractionSteps::Beta);
-
-        self.extract_step(ProofExtractionSteps::Gamma);
-
-        let nb_permutation_commitments = vk.cs().permutation().columns.chunks(chunk_len).len();
-
-        (0..nb_permutation_commitments).for_each(|_| {
-            self.extract_step(ProofExtractionSteps::PermutationsCommitted);
-        });
-
-        (0..nb_lookups).for_each(|_| self.extract_step(ProofExtractionSteps::LookupCommitment));
-
-        self.extract_step(ProofExtractionSteps::VanishingRand);
-
-        self.extract_step(ProofExtractionSteps::YCoordinate);
-
-        (0..vk.get_domain().get_quotient_poly_degree()).for_each(|_| {
-            self.extract_step(ProofExtractionSteps::VanishingSplit);
-        });
-
-        self.extract_step(ProofExtractionSteps::XCoordinate);
-
-        (0..vk.cs().advice_queries().len()).for_each(|_| {
-            self.extract_step(ProofExtractionSteps::AdviceEval);
-        });
-
-        (0..vk.cs().fixed_queries().len()).for_each(|_| {
-            self.extract_step(ProofExtractionSteps::FixedEval);
-        });
-
-        self.extract_step(ProofExtractionSteps::RandomEval);
-
-        // for each commitment do a PermutationCommon
-        #[cfg(feature = "plutus_debug")]
-        info!("nb PermCom: {}", vk.permutation().commitments().len());
-
-        vk.permutation()
-            .commitments()
-            .iter()
-            .enumerate()
-            .for_each(|_| {
-                self.extract_step(ProofExtractionSteps::PermutationCommon);
-            });
-
-        let letters = 'a'..='z';
-        let last_index = nb_permutation_commitments - 1;
-        (0..nb_permutation_commitments)
-            .zip(letters)
-            .enumerate()
-            .for_each(|(index, (_, letter))| {
-                self.extract_permutation_eval(letter);
-                self.extract_permutation_eval(letter);
-
-                if index != last_index {
-                    self.extract_permutation_eval(letter);
-                }
-            });
-
-        (0..nb_lookups).for_each(|_| self.extract_step(ProofExtractionSteps::LookupEval));
+        for (phase, _challenge) in vk.cs().challenge_phase().iter().zip(challenges.iter_mut()) {
+            if current_phase == *phase {
+                circuit_repr.extract_step(ProofExtractionSteps::SqueezeChallenge);
+            }
+        }
     }
+
+    circuit_repr.extract_step(ProofExtractionSteps::Theta);
+
+    let nb_lookups = vk.cs().lookups().len();
+    (0..nb_lookups).for_each(|_argument| {
+        circuit_repr.extract_step(ProofExtractionSteps::LookupPermuted);
+    });
+
+    circuit_repr.extract_step(ProofExtractionSteps::Beta);
+
+    circuit_repr.extract_step(ProofExtractionSteps::Gamma);
+
+    let nb_permutation_commitments = vk.cs().permutation().columns.chunks(chunk_len).len();
+
+    (0..nb_permutation_commitments).for_each(|_| {
+        circuit_repr.extract_step(ProofExtractionSteps::PermutationsCommitted);
+    });
+
+    (0..nb_lookups).for_each(|_| circuit_repr.extract_step(ProofExtractionSteps::LookupCommitment));
+
+    circuit_repr.extract_step(ProofExtractionSteps::VanishingRand);
+
+    circuit_repr.extract_step(ProofExtractionSteps::YCoordinate);
+
+    (0..vk.get_domain().get_quotient_poly_degree()).for_each(|_| {
+        circuit_repr.extract_step(ProofExtractionSteps::VanishingSplit);
+    });
+
+    circuit_repr.extract_step(ProofExtractionSteps::XCoordinate);
+
+    (0..vk.cs().advice_queries().len()).for_each(|_| {
+        circuit_repr.extract_step(ProofExtractionSteps::AdviceEval);
+    });
+
+    (0..vk.cs().fixed_queries().len()).for_each(|_| {
+        circuit_repr.extract_step(ProofExtractionSteps::FixedEval);
+    });
+
+    circuit_repr.extract_step(ProofExtractionSteps::RandomEval);
+
+    // for each commitment do a PermutationCommon
+    #[cfg(feature = "plutus_debug")]
+    info!("nb PermCom: {}", vk.permutation().commitments().len());
+
+    vk.permutation()
+        .commitments()
+        .iter()
+        .enumerate()
+        .for_each(|_| {
+            circuit_repr.extract_step(ProofExtractionSteps::PermutationCommon);
+        });
+
+    let letters = 'a'..='z';
+    let last_index = nb_permutation_commitments - 1;
+    (0..nb_permutation_commitments)
+        .zip(letters)
+        .enumerate()
+        .for_each(|(index, (_, letter))| {
+            circuit_repr.extract_permutation_eval(letter);
+            circuit_repr.extract_permutation_eval(letter);
+
+            if index != last_index {
+                circuit_repr.extract_permutation_eval(letter);
+            }
+        });
+
+    (0..nb_lookups).for_each(|_| circuit_repr.extract_step(ProofExtractionSteps::LookupEval));
 }
--- src/plutus_gen/extraction/data/expression_steps/vanishing.rs
+++ src/plutus_gen/extraction/data/expression_steps/vanishing.rs
@@ -4,51 +4,54 @@ use crate::plutus_gen::extraction::pcs::ExtractPCS;
 #[cfg(feature = "plutus_debug")]
 use log::info;
 
-impl<PCS: ExtractPCS> CircuitRepresentation<PCS> {
-    pub fn vanishing_expressions(&mut self) {
-        // let mut terms = Vec::new();
+pub fn vanishing_expressions<PCS>(circuit_repr: &mut CircuitRepresentation<PCS>)
+where
+    PCS: ExtractPCS,
+{
+    let nb_vanishing_splits = circuit_repr.nb_vanishing_splits();
 
-        let nb_vanishing_splits = self.nb_vanishing_splits();
+    // Raphael: Not sure this works for more splits
+    // !hCommitment1 = scale xn_one G1_zero + vanishingSplit{:?}", nb_vanishing_splits
+    // a + b
+    {
+        let a = ExpressionG1::Scale(
+            Box::new(ExpressionG1::Zero),
+            ScalarExpression::Variable(XN_STR.to_string()),
+        );
+        let b = ExpressionG1::VanishingSplit(nb_vanishing_splits);
+        let init_expr = ExpressionG1::Sum(Box::new(a), Box::new(b));
+        // terms.push((h_com_str(1), term));
+        circuit_repr.expressions.vanishing(h_com_str(1), init_expr);
+    }
 
-        // Raphael: Not sure this works for more splits
-        // !hCommitment1 = scale xn_one G1_zero + vanishingSplit{:?}", nb_vanishing_splits
-        // a + b
-        {
-            let a = ExpressionG1::Scale(
-                Box::new(ExpressionG1::Zero),
-                ScalarExpression::Variable(XN_STR.to_string()),
-            );
-            let b = ExpressionG1::VanishingSplit(nb_vanishing_splits);
-            let init_expr = ExpressionG1::Sum(Box::new(a), Box::new(b));
-            // terms.push((h_com_str(1), term));
-            self.expressions.vanishing(h_com_str(1), init_expr);
-        }
+    // render last on as vanishing_g
+    // !hCommitment{:?} = scale xn hCommitment{:?} + vanishingSplit{:?}
+    // a + b
+    for i in 1..(nb_vanishing_splits - 1) {
+        let a = ExpressionG1::Scale(
+            Box::new(ExpressionG1::Variable(h_com_str(i))),
+            ScalarExpression::Variable(XN_STR.to_string()),
+        );
+        let b = ExpressionG1::VanishingSplit(nb_vanishing_splits - i);
+        let loop_expr = ExpressionG1::Sum(Box::new(a), Box::new(b));
 
-        // render last on as vanishing_g
-        // !hCommitment{:?} = scale xn hCommitment{:?} + vanishingSplit{:?}
-        // a + b
-        for i in 1..(nb_vanishing_splits - 1) {
-            let a = ExpressionG1::Scale(
-                Box::new(ExpressionG1::Variable(h_com_str(i))),
-                ScalarExpression::Variable(XN_STR.to_string()),
-            );
-            let b = ExpressionG1::VanishingSplit(nb_vanishing_splits - i);
-            let loop_expr = ExpressionG1::Sum(Box::new(a), Box::new(b));
+        // terms.push((h_com_str(i + 1), term));
+        circuit_repr
+            .expressions
+            .vanishing(h_com_str(i + 1), loop_expr);
+    }
 
-            // terms.push((h_com_str(i + 1), term));
-            self.expressions.vanishing(h_com_str(i + 1), loop_expr);
-        }
-
-        // !vanishing_g = scale xn hCommitment{} + vanishingSplit1; nb_vanishing_splits - 1
-        // a + b
-        {
-            let a = ExpressionG1::Scale(
-                Box::new(ExpressionG1::Variable(h_com_str(nb_vanishing_splits - 1))),
-                ScalarExpression::Variable(XN_STR.to_string()),
-            );
-            let b = ExpressionG1::VanishingSplit(1);
-            let g_expr = ExpressionG1::Sum(Box::new(a), Box::new(b));
-            self.expressions.vanishing(VANISH_G_STR.to_string(), g_expr);
-        }
+    // !vanishing_g = scale xn hCommitment{} + vanishingSplit1; nb_vanishing_splits - 1
+    // a + b
+    {
+        let a = ExpressionG1::Scale(
+            Box::new(ExpressionG1::Variable(h_com_str(nb_vanishing_splits - 1))),
+            ScalarExpression::Variable(XN_STR.to_string()),
+        );
+        let b = ExpressionG1::VanishingSplit(1);
+        let g_expr = ExpressionG1::Sum(Box::new(a), Box::new(b));
+        circuit_repr
+            .expressions
+            .vanishing(VANISH_G_STR.to_string(), g_expr);
     }
 }
