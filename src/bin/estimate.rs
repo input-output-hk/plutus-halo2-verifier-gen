b5cdbcb4df1239d16495f3c414f6d513ab83b790
Raphael Toledo <raphael.toledo@iohk.io>
2026-07-02 15:49:20 +0100

    Removing unneeded wasm guards

--- src/bin/estimate.rs
+++ src/bin/estimate.rs
@@ -1,18 +1,13 @@
-#[cfg(not(target_arch = "wasm32"))]
 use clap::Parser;
-#[cfg(not(target_arch = "wasm32"))]
 use plutus_halo2_verifier_gen::plutus_gen::{EstimateCliArguments, estimate_cost};
 
 fn main() {
-    #[cfg(not(target_arch = "wasm32"))]
-    {
-        let args = EstimateCliArguments::parse();
-        estimate_cost(
-            args.nb_public_inputs,
-            usize::from(args.committed_instances),
-            args.recursion,
-            args.config(),
-            &args.chips(),
-        );
-    }
+    let args = EstimateCliArguments::parse();
+    estimate_cost(
+        args.nb_public_inputs,
+        usize::from(args.committed_instances),
+        args.recursion,
+        args.config(),
+        &args.chips(),
+    );
 }
